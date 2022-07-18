783035db94064f398476298482a19c2e948c2aed
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-07-18 23:50:34 +0530

    update Customer index.jsx

--- src/pages/Customer/index.jsx
+++ src/pages/Customer/index.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
+import {styled, useTheme} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -16,14 +16,19 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import MainPanel from "../Main";
+import CarCrashIcon from '@mui/icons-material/CarCrash';
+import SecurityUpdateGoodIcon from '@mui/icons-material/SecurityUpdateGood';
+import InfoIcon from '@mui/icons-material/Info';
+import CarDetails from "./CarDetail";
+import RequestDetails from "./RequestDetails";
+import UpdateInformation from "./UpdateInformation";
+
 
 const drawerWidth = 240;
 
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
+const Main = styled('main', {shouldForwardProp: (prop) => prop !== 'open'})(
+    ({theme, open}) => ({
         flexGrow: 1,
         padding: theme.spacing(3),
         transition: theme.transitions.create('margin', {
@@ -43,7 +48,7 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
 
 const AppBar = styled(MuiAppBar, {
     shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
+})(({theme, open}) => ({
     transition: theme.transitions.create(['margin', 'width'], {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.leavingScreen,
@@ -58,7 +63,7 @@ const AppBar = styled(MuiAppBar, {
     }),
 }));
 
-const DrawerHeader = styled('div')(({ theme }) => ({
+const DrawerHeader = styled('div')(({theme}) => ({
     display: 'flex',
     alignItems: 'center',
     padding: theme.spacing(0, 1),
@@ -81,18 +86,18 @@ export default function CustomerPanel() {
 
     return (
         <MainPanel>
-            <Box sx={{ flexGrow: 1  }}>
-                <CssBaseline />
-                <AppBar position="static" open={open} sx={{backgroundColor:"darkblue",marginTop:-5}}>
+            <Box sx={{flexGrow: 1}}>
+                <CssBaseline/>
+                <AppBar position="static" open={open} sx={{backgroundColor: "darkblue", marginTop: -5}}>
                     <Toolbar>
                         <IconButton
                             color="inherit"
                             aria-label="open drawer"
                             onClick={handleDrawerOpen}
                             edge="start"
-                            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+                            sx={{mr: 2, ...(open && {display: 'none'})}}
                         >
-                            <MenuIcon />
+                            <MenuIcon/>
                         </IconButton>
                         <Typography variant="h6" noWrap component="div">
                             Customer
@@ -113,28 +118,35 @@ export default function CustomerPanel() {
                     open={open}
                 >
                     <DrawerHeader>
+                        <Typography variant="h6" noWrap component="div"  sx={{position:"absolute",left:55,fontWeight:"bold"}}>
+                            DashBoard
+                        </Typography>
+
                         <IconButton onClick={handleDrawerClose}>
-                            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                            {theme.direction === 'ltr' ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
                         </IconButton>
+
                     </DrawerHeader>
-                    <Divider />
+                    <Divider/>
                     <List>
-                        {['place rental request', 'Update Details', 'status of the rental request'].map((text, index) => (
+                        {['Car Details','Update Information','Request Details'].map((text, index) => (
                             <ListItem key={text} disablePadding>
                                 <ListItemButton>
                                     <ListItemIcon>
-                                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                                        {index === 0 ? <CarCrashIcon/>:
+                                            index ===1 ? <SecurityUpdateGoodIcon/>:
+                                                <InfoIcon/> }
                                     </ListItemIcon>
-                                    <ListItemText primary={text} />
+                                    <ListItemText primary={text}/>
                                 </ListItemButton>
                             </ListItem>
                         ))}
                     </List>
                 </Drawer>
-                <Main open={open}>
-                    <DrawerHeader />
-                </Main>
             </Box>
+           {/*<CarDetails/>*/}
+           {/*<RequestDetails/>*/}
+           <UpdateInformation/>
         </MainPanel>
     );
 }
