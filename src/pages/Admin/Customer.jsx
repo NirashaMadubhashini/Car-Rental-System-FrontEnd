50e0be77e7f79198df70c048038765078cecefea
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-07-22 00:50:08 +0530

    update Admin Customer.jsx

--- src/pages/Admin/Customer.jsx
+++ src/pages/Admin/Customer.jsx
@@ -1,32 +1,26 @@
 import React from 'react'
 import Box from "@mui/material/Box";
-import Table from "@mui/material/Table";
 import Button from "@mui/material/Button";
 import Tables from "../../component/common/Table/table";
 import Typography from "@mui/material/Typography";
 
-const ManageCustomer = ({}) => {
-    function createData(name, calories, fat, carbs, protein) {
-        return {name, calories, fat, carbs, protein};
-    }
 
-    const rows = [
-        createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-        createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-        createData('Eclair', 262, 16.0, 24, 6.0),
-        createData('Cupcake', 305, 3.7, 67, 4.3),
-        createData('Gingerbread', 356, 16.0, 49, 3.9),
-    ];
+
+
+const ManageCustomer = ({}) => {
 
     return (
+
         <div>
-            <Typography sx={{marginLeft:40,fontSize:35,fontWeight:'bold',top:100}}>
+            <Typography sx={{marginLeft: 40, fontSize: 35, fontWeight: 'bold', top: 100}}>
                 Customer
             </Typography>
+
             <Box sx={{bgcolor: 'white', height: '100vh', flexGrow: 1}}>
                 <div>
-                    <div >
-                        <Button variant="contained" color="primary" sx={{position: "absolute", top: 180, left: 25}}>
+                    <div>
+                        <Button variant="contained" color="primary" label="save" type="submit"
+                                sx={{position: "absolute", top: 180, left: 25}}>
                             Add Car
                         </Button>
 
@@ -34,9 +28,7 @@ const ManageCustomer = ({}) => {
                             Add to Maintain
                         </Button>
                     </div>
-
                     <Tables/>
-
                 </div>
             </Box>
         </div>
