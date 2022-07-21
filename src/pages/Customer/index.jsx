d8f177de9a07513b32c546fa83cbd11b67e65f02
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-07-22 00:53:51 +0530

    update Customer index.jsx

--- src/pages/Customer/index.jsx
+++ src/pages/Customer/index.jsx
@@ -187,7 +187,7 @@ export default function CustomerPanel() {
                 isCarDetail ? <CarDetails/> :
                     isUpdateInformation ? <UpdateInformation/> :
                         isRequestDetails ? <RequestDetails/> :
-                            ' '
+                            'Customer'
             }
         </MainPanel>
     );
