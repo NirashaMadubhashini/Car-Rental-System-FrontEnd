42f314e3bdda968ed9ed66132ddf54f7bef0c9cc
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-07-23 21:50:48 +0530

    update AdminDashBoard.jsx

--- src/pages/Admin/AdminDashBoard.jsx
+++ src/pages/Admin/AdminDashBoard.jsx
@@ -7,58 +7,6 @@ import CardContent from "@mui/material/CardContent";
 import Card from "@mui/material/Card";
 
 
-const bull = (
-    <Box
-        component="span"
-        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-    >
-        •
-    </Box>
-);
-
-const StyledBadge = styled(BadgeUnstyled)`
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  font-size: 14px;
-  list-style: none;
-  font-family: IBM Plex Sans, sans-serif;
-  position: relative;
-  display: inline-block;
-  line-height: 1;
-
-  & .${badgeUnstyledClasses.badge} {
-    z-index: auto;
-    min-width: 20px;
-    height: 20px;
-    padding: 0 6px;
-    color: #fff;
-    font-weight: 400;
-    font-size: 12px;
-    line-height: 20px;
-    white-space: nowrap;
-    text-align: center;
-    background: #07f;
-    border-radius: 10px;
-    box-shadow: 0 0 0 1px #fff;
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(50%, -50%);
-    transform-origin: 100% 0;
-  }
-`;
-
-function notificationsLabel(count) {
-    if (count === 0) {
-        return 'no notifications';
-    }
-    if (count > 99) {
-        return 'more than 99 notifications';
-    }
-    return `${count} notifications`;
-}
-
 const AdminDashBoard = ({})=>{
 
     return (
@@ -69,11 +17,7 @@ const AdminDashBoard = ({})=>{
                 </Typography>
 
             </CardContent>
-            <div aria-label={notificationsLabel(100)}>
-                <StyledBadge badgeContent={100}>
-                    <MailIcon />
-                </StyledBadge>
-            </div>
+
         </Card>
     );
 
