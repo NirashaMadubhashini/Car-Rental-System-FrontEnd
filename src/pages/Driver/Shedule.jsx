243ae912457d39df30757a4ea3e6064cbc365474
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-07-25 08:30:30 +0530

    update Driver Shedule.jsx

--- src/pages/Driver/Shedule.jsx
+++ src/pages/Driver/Shedule.jsx
@@ -1,30 +1,343 @@
 import React from 'react'
 import Box from "@mui/material/Box";
-import Table from "@mui/material/Table";
 import Button from "@mui/material/Button";
-import Tables from "../../component/common/Table/table";
-import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import {Grid, IconButton} from "@mui/material";
+import TextField from "@mui/material/TextField";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import RubberBtn from "../../component/common/RubberBandBtn";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import {visuallyHidden} from "@mui/utils";
+import PropTypes from "prop-types";
+import Paper from "@mui/material/Paper";
+import TableContainer from "@mui/material/TableContainer";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import CreateIcon from "@mui/icons-material/Create";
+import DeleteIcon from "@mui/icons-material/Delete";
+import TablePagination from "@mui/material/TablePagination";
+
+
+function createData(nicNumber, name, drivingLicenseNumber,location,pickupDate,pickupTime,returnDate,returnTime) {
+    return {
+        nicNumber, name, drivingLicenseNumber,location,pickupDate,pickupTime,returnDate,returnTime
+    };
+}
+
+const rows = [
+    createData("1234567", "Praboda", "ABC123","Baddegama", "2022.07.24","12.00pm","2022.07.26","12.00pm"),
+    createData("7654321", "Sadali", "ABC456", "Hikkaduwa", "2022.07.26","13.00pm","2022.07.27","10.00am"),
+    createData("6543218", "Geethika", "ABC789", "Yakkalamulla", "2022.07.24","08.00am","2022.07.28","09.00pm"),
+    createData("5432198", "Anupama", "DEF123", "Karapitiya", "2022.07.24","10.00am","2022.07.29","10.00am")
+];
+
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
+
+function getComparator(order, orderBy) {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+
+function stableSort(array, comparator) {
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+        const order = comparator(a[0], b[0]);
+        if (order !== 0) {
+            return order;
+        }
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+    {
+        id: 'nicNumber',
+        numeric: false,
+        disablePadding: true,
+        label: 'NIC Number',
+    },
+    {
+        id: 'name',
+        numeric: false,
+        disablePadding: true,
+        label: 'Name',
+    },
+    {
+        id: 'drivingLicenseNumber',
+        numeric: false,
+        disablePadding: true,
+        label: 'License Number',
+    },
+    {
+        id: 'location',
+        numeric: false,
+        disablePadding: true,
+        label: 'Location',
+    },
+    {
+        id: 'pickupDate',
+        numeric: false,
+        disablePadding: true,
+        label: 'Pick-up Date',
+    },
+    {
+        id: 'pickupTime',
+        numeric: false,
+        disablePadding: true,
+        label: 'Pick-up Time',
+    },
+    {
+        id: 'returnDate',
+        numeric: false,
+        disablePadding: true,
+        label: 'Return Date',
+    },
+    {
+        id: 'returnTime',
+        numeric: false,
+        disablePadding: true,
+        label: 'Return Time',
+    },
+];
+
+function EnhancedTableHead(props) {
+    const {onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort} =
+        props;
+    const createSortHandler = (property) => (event) => {
+        onRequestSort(event, property);
+    };
+    return (
+        <TableHead>
+            <TableRow>
+                <TableCell padding="checkbox">
+
+                </TableCell>
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        padding={headCell.disablePadding ? 'none' : 'normal'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <Box component="span" sx={visuallyHidden}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                </Box>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+EnhancedTableHead.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+    onRequestSort: PropTypes.func.isRequired,
+    onSelectAllClick: PropTypes.func.isRequired,
+    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+    orderBy: PropTypes.string.isRequired,
+    rowCount: PropTypes.number.isRequired,
+};
+
+const EnhancedTableToolbar = (props) => {
+    const {numSelected} = props;
+
+};
+
+EnhancedTableToolbar.propTypes = {
+    numSelected: PropTypes.number.isRequired,
+};
+
 
 const Shedule = ({}) => {
+
+    const [order, setOrder] = React.useState('asc');
+    const [orderBy, setOrderBy] = React.useState('calories');
+    const [selected, setSelected] = React.useState([]);
+    const [page, setPage] = React.useState(0);
+    const [dense, setDense] = React.useState(false);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const handleRequestSort = (event, property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const handleSelectAllClick = (event) => {
+        if (event.target.checked) {
+            const newSelecteds = rows.map((n) => n.name);
+            setSelected(newSelecteds);
+            return;
+        }
+        setSelected([]);
+    };
+
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+
+    const isSelected = (name) => selected.indexOf(name) !== -1;
+
+
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
     return (
+
         <div>
-            <Typography sx={{marginLeft:40,fontSize:35,fontWeight:'bold',top:100}}>
-                Shedule
-            </Typography>
-            <Box sx={{bgcolor: 'white', height: '100vh', flexGrow: 1}}>
+            <Grid item lg={12} xs={12} sm={12} md={12}>
+                <RubberBtn name="Shedule"/>
+            </Grid>
+            <Divider/>
+
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': {},
+                }}
+                noValidate
+                autoComplete="off"
+            >
+
                 <div>
-                    <div >
-                        <Button variant="contained" color="primary" sx={{position: "absolute", top: 180, left: 25}}>
-                            Add Car
-                        </Button>
+                    <Box sx={{width: '100%'}}>
+                        <Paper sx={{width: '100%', mb: 2}}>
+                            <EnhancedTableToolbar numSelected={selected.length}/>
+                            <TableContainer>
+                                <Table
+                                    sx={{minWidth: 750, marginTop: 5}}
+                                    aria-labelledby="tableTitle"
+                                    size={dense ? 'small' : 'medium'}
+                                >
+                                    <EnhancedTableHead
+                                        numSelected={selected.length}
+                                        order={order}
+                                        orderBy={orderBy}
+                                        onSelectAllClick={handleSelectAllClick}
+                                        onRequestSort={handleRequestSort}
+                                        rowCount={rows.length}
+                                    />
+                                    <TableBody>
+                                        {stableSort(rows, getComparator(order, orderBy))
+                                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                            .map((row, index) => {
+                                                const isItemSelected = isSelected(row.nicNumber);
+                                                const labelId = `enhanced-table-checkbox-${index}`;
 
-                        <Button variant="contained" color="success" sx={{position: "absolute", top: 180, left: 130}}>
-                            Add to Maintain
-                        </Button>
-                    </div>
+                                                return (
+                                                    <TableRow
+                                                        hover
+                                                        aria-checked={isItemSelected}
+                                                        tabIndex={-1}
+                                                        key={row.nicNumber}
+                                                        selected={isItemSelected}
+                                                    >
+                                                        <TableCell>
+                                                        </TableCell>
+                                                        <TableCell
+                                                            component="th"
+                                                            id={labelId}
+                                                            scope="row"
+                                                            padding="none"
+                                                        >
+                                                            {row.nicNumber}
+                                                        </TableCell>
+                                                        <TableCell component="th"
+                                                                   id={labelId}
+                                                                   scope="row"
+                                                                   padding="none">{row.name}
+                                                        </TableCell>
+                                                        <TableCell component="th"
+                                                                   id={labelId}
+                                                                   scope="row"
+                                                                   padding="none">{row.drivingLicenseNumber}
+                                                        </TableCell>
+                                                        <TableCell component="th"
+                                                                   id={labelId}
+                                                                   scope="row"
+                                                                   padding="none">{row.location}
+                                                        </TableCell>
+                                                        <TableCell component="th"
+                                                                   id={labelId}
+                                                                   scope="row"
+                                                                   padding="none">{row.pickupDate}
+                                                        </TableCell>
+                                                        <TableCell component="th"
+                                                                   id={labelId}
+                                                                   scope="row"
+                                                                   padding="none">{row.pickupTime}
+                                                        </TableCell>
+                                                        <TableCell component="th"
+                                                                   id={labelId}
+                                                                   scope="row"
+                                                                   padding="none">{row.returnDate}
+                                                        </TableCell>
+                                                        <TableCell component="th"
+                                                                   id={labelId}
+                                                                   scope="row"
+                                                                   padding="none">{row.returnTime}
+                                                        </TableCell>
 
 
-                    <Tables/>
+                                                    </TableRow>
+                                                );
+                                            })}
+                                        {emptyRows > 0 && (
+                                            <TableRow
+                                                style={{
+                                                    height: (dense ? 33 : 53) * emptyRows,
+                                                }}
+                                            >
+                                                <TableCell colSpan={6}/>
+                                            </TableRow>
+                                        )}
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                            <TablePagination
+                                rowsPerPageOptions={[5, 10, 25]}
+                                component="div"
+                                count={rows.length}
+                                rowsPerPage={rowsPerPage}
+                                page={page}
+                                onPageChange={handleChangePage}
+                                onRowsPerPageChange={handleChangeRowsPerPage}
+                            />
+                        </Paper>
+                    </Box>
+
+
                 </div>
             </Box>
         </div>
