602c06ef7690109bdd5a41f27d323e77ae3c2979
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-07-25 10:19:18 +0530

    update Customer PlaceRequest.jsx

--- src/pages/Customer/PlaceRequest.jsx
+++ src/pages/Customer/PlaceRequest.jsx
@@ -23,210 +23,11 @@ import CreateIcon from "@mui/icons-material/Create";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ConstructionIcon from "@mui/icons-material/Construction";
 import TablePagination from "@mui/material/TablePagination";
-
-
-
-function createData(userName,password,drivingLicenseNumber,drivingLicensePhoto,address,contactNumber,update,deleted) {
-    return {
-        userName,
-        password,
-        drivingLicenseNumber,
-        drivingLicensePhoto,
-        address,
-        contactNumber,
-        update,
-        deleted
-    };
-}
-
-const rows = [
-    createData('nirasha@', "nirasha",  "ABC123","","Galle","01234567"),
-    createData('geeth@', "geeth", "ABC456","","Kalegana","02345678"),
-    createData('hansi@', "hansi",  "ABC789","","Matara","03456789"),
-    createData('chamodi@', "chamodi",  "DEF123","","RichmondHill","04567891"),
-    createData('milasha@', "milasha",  "DEF456","","Mirissa","05678912"),
-
-];
-
-function descendingComparator(a, b, orderBy) {
-    if (b[orderBy] < a[orderBy]) {
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-        return 1;
-    }
-    return 0;
-}
-
-function getComparator(order, orderBy) {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-
-function stableSort(array, comparator) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) {
-            return order;
-        }
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
-}
-
-const headCells = [
-    {
-        id: 'userName',
-        numeric: false,
-        disablePadding: true,
-        label: 'UserName',
-    },
-    {
-        id: 'password',
-        numeric: false,
-        disablePadding: true,
-        label: 'Password',
-    },
-    {
-        id: 'drivingLicenseNumber',
-        numeric: false,
-        disablePadding: true,
-        label: 'Driving License Number',
-    },
-    {
-        id: 'drivingLicensePhoto',
-        numeric: false,
-        disablePadding: true,
-        label: 'Driving License Photo',
-    },
-    {
-        id: 'address',
-        numeric: false,
-        disablePadding: true,
-        label: 'Address',
-    },
-    {
-        id: 'contactNumber',
-        numeric: false,
-        disablePadding: true,
-        label: 'Contact Number',
-    },
-    {
-        id: 'update',
-        numeric: false,
-        disablePadding: true,
-        label: 'Update',
-    },
-    {
-        id: 'deleted',
-        numeric: false,
-        disablePadding: true,
-        label: 'Delete',
-    },
-];
-
-function EnhancedTableHead(props) {
-    const {onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort} =
-        props;
-    const createSortHandler = (property) => (event) => {
-        onRequestSort(event, property);
-    };
-    return (
-        <TableHead>
-            <TableRow>
-                <TableCell padding="checkbox">
-
-                </TableCell>
-                {headCells.map((headCell) => (
-                    <TableCell
-                        key={headCell.id}
-                        align={headCell.numeric ? 'right' : 'left'}
-                        padding={headCell.disablePadding ? 'none' : 'normal'}
-                        sortDirection={orderBy === headCell.id ? order : false}
-                    >
-                        <TableSortLabel
-                            active={orderBy === headCell.id}
-                            direction={orderBy === headCell.id ? order : 'asc'}
-                            onClick={createSortHandler(headCell.id)}
-                        >
-                            {headCell.label}
-                            {orderBy === headCell.id ? (
-                                <Box component="span" sx={visuallyHidden}>
-                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                                </Box>
-                            ) : null}
-                        </TableSortLabel>
-                    </TableCell>
-                ))}
-            </TableRow>
-        </TableHead>
-    );
-}
-
-EnhancedTableHead.propTypes = {
-    numSelected: PropTypes.number.isRequired,
-    onRequestSort: PropTypes.func.isRequired,
-    onSelectAllClick: PropTypes.func.isRequired,
-    order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-    orderBy: PropTypes.string.isRequired,
-    rowCount: PropTypes.number.isRequired,
-};
-
-const EnhancedTableToolbar = (props) => {
-    const {numSelected} = props;
-
-};
-
-EnhancedTableToolbar.propTypes = {
-    numSelected: PropTypes.number.isRequired,
-};
-
+import Autocomplete from "@mui/material/Autocomplete";
+import PlaceRequestTables from "./placeRequestTable";
 
 
 const PlaceRequest = ({}) => {
-
-    const [order, setOrder] = React.useState('asc');
-    const [orderBy, setOrderBy] = React.useState('calories');
-    const [selected, setSelected] = React.useState([]);
-    const [page, setPage] = React.useState(0);
-    const [dense, setDense] = React.useState(false);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-    const handleRequestSort = (event, property) => {
-        const isAsc = orderBy === property && order === 'asc';
-        setOrder(isAsc ? 'desc' : 'asc');
-        setOrderBy(property);
-    };
-
-    const handleSelectAllClick = (event) => {
-        if (event.target.checked) {
-            const newSelecteds = rows.map((n) => n.name);
-            setSelected(newSelecteds);
-            return;
-        }
-        setSelected([]);
-    };
-
-
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
-
-
-    const isSelected = (name) => selected.indexOf(name) !== -1;
-
-
-    const emptyRows =
-        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
-
     return (
 
         <div>
@@ -248,60 +49,65 @@ const PlaceRequest = ({}) => {
                       sx={{paddingLeft: 5,mt:5}}
                 >
                     <Grid item>
-                        <TextField id="outlined-basic" label="UserName" variant="outlined"
-                                   helperText="Enter UserName" name="userName"
+                        <Autocomplete
+                            disablePortal
+                            id="combo-box-demo"
+                            options={cars}
+                            sx={{ width: 250,mt:-3 }}
+                            renderInput={(params) => <TextField {...params} label="Car Names" />}
                         />
                     </Grid>
+
                     <Grid item>
                         <TextField
-                            helperText="EnterPassword"
+                            helperText="Enter Pick-up Date"
                             variant="outlined"
                             id="outlined-basic"
-                            label="Password"
-                            name="password"
+                            label="Pick-up Date"
+                            name="pickupDate"
 
                         />
                     </Grid>
                     <Grid item>
                         <TextField
-                            helperText="Enter Driving License number"
+                            helperText="Enter Pick-up Time"
                             id="demo-helper-text-aligned"
-                            label="Driving License number"
-                            name="drivingLicenseNumber"
+                            label="Pick-up Time"
+                            name="pickupTime"
 
                         />
                     </Grid>
                     <Grid item>
                         <TextField
-                            helperText="Upload Driving License Photo"
+                            helperText="Enter Return Date"
                             id="demo-helper-text-aligned"
-                            label="Driving License Photo"
-                            name="drivingLicensePhoto"
+                            label="Return Date"
+                            name="returnDate"
                         />
                     </Grid>
                     <Grid item>
                         <TextField
-                            helperText="Enter Address"
+                            helperText="Enter Return Time"
                             id="demo-helper-text-aligned"
-                            label="Address"
-                            name="address"
+                            label="Return Time"
+                            name="returnTime"
 
                         />
                     </Grid>
                     <Grid item>
                         <TextField
-                            helperText="Enter Contact Number"
+                            helperText="Enter Location"
                             id="demo-helper-text-aligned"
-                            label="Contact Number"
-                            name="contactNumber"
+                            label="Location"
+                            name="location"
 
                         />
                     </Grid>
                 </Grid>
                 <InputBase
                     sx={{ml: 10, mt: 5, flex: 1}}
-                    placeholder="Search NIC Number"
-                    inputProps={{'aria-label': 'search NIC Number'}}
+                    placeholder="Search Car Name"
+                    inputProps={{'aria-label': 'search Car Name'}}
                     variant="standard"
                 />
                 <IconButton type="submit" sx={{p:'20px'}} aria-label="search">
@@ -315,7 +121,7 @@ const PlaceRequest = ({}) => {
                         </Button>
                         <Button  size="medium" type="submit" variant="contained"
                                  sx={{ml:3, mt: -13}}>
-                            Add Details
+                            Place Request
                         </Button>
                         <Button type="reset" variant="contained" color="success"
                                 sx={{ml: 3, mt: -13}}>
@@ -323,112 +129,7 @@ const PlaceRequest = ({}) => {
                         </Button>
                     </div>
 
-                    <Box sx={{width: '100%'}}>
-                        <Paper sx={{width: '100%', mb: 2}}>
-                            <EnhancedTableToolbar numSelected={selected.length}/>
-                            <TableContainer>
-                                <Table
-                                    sx={{minWidth: 750, marginTop: 5}}
-                                    aria-labelledby="tableTitle"
-                                    size={dense ? 'small' : 'medium'}
-                                >
-                                    <EnhancedTableHead
-                                        numSelected={selected.length}
-                                        order={order}
-                                        orderBy={orderBy}
-                                        onSelectAllClick={handleSelectAllClick}
-                                        onRequestSort={handleRequestSort}
-                                        rowCount={rows.length}
-                                    />
-                                    <TableBody>
-                                        {stableSort(rows, getComparator(order, orderBy))
-                                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                            .map((row, index) => {
-                                                const isItemSelected = isSelected(row.userName);
-                                                const labelId = `enhanced-table-checkbox-${index}`;
-
-                                                return (
-                                                    <TableRow
-                                                        hover
-                                                        aria-checked={isItemSelected}
-                                                        tabIndex={-1}
-                                                        key={row.userName}
-                                                        selected={isItemSelected}
-                                                    >
-                                                        <TableCell>
-                                                        </TableCell>
-                                                        <TableCell
-                                                            component="th"
-                                                            id={labelId}
-                                                            scope="row"
-                                                            padding="none"
-                                                        >
-                                                            {row.userName}
-                                                        </TableCell>
-                                                        <TableCell component="th"
-                                                                   id={labelId}
-                                                                   scope="row"
-                                                                   padding="none">{row.password}
-                                                        </TableCell>
-                                                        <TableCell component="th"
-                                                                   id={labelId}
-                                                                   scope="row"
-                                                                   padding="none">{row.drivingLicenseNumber}
-                                                        </TableCell>
-                                                        <TableCell component="th"
-                                                                   id={labelId}
-                                                                   scope="row"
-                                                                   padding="none">{row.drivingLicensePhoto}
-                                                        </TableCell>
-                                                        <TableCell component="th"
-                                                                   id={labelId}
-                                                                   scope="row"
-                                                                   padding="none">{row.address}
-                                                        </TableCell>
-                                                        <TableCell component="th"
-                                                                   id={labelId}
-                                                                   scope="row"
-                                                                   padding="none">{row.contactNumber}
-                                                        </TableCell>
-
-                                                        <TableCell component="th"
-                                                                   id={labelId}
-                                                                   scope="row"
-                                                                   padding="none">{row.update}
-                                                            <CreateIcon/>
-                                                        </TableCell>
-                                                        <TableCell component="th"
-                                                                   id={labelId}
-                                                                   scope="row"
-                                                                   padding="none">{row.delete}
-                                                            <DeleteIcon/>
-                                                        </TableCell>
-                                                    </TableRow>
-                                                );
-                                            })}
-                                        {emptyRows > 0 && (
-                                            <TableRow
-                                                style={{
-                                                    height: (dense ? 33 : 53) * emptyRows,
-                                                }}
-                                            >
-                                                <TableCell colSpan={6}/>
-                                            </TableRow>
-                                        )}
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                            <TablePagination
-                                rowsPerPageOptions={[5, 10, 25]}
-                                component="div"
-                                count={rows.length}
-                                rowsPerPage={rowsPerPage}
-                                page={page}
-                                onPageChange={handleChangePage}
-                                onRowsPerPageChange={handleChangeRowsPerPage}
-                            />
-                        </Paper>
-                    </Box>
+                    <PlaceRequestTables/>
 
                 </div>
             </Box>
@@ -436,5 +137,11 @@ const PlaceRequest = ({}) => {
     )
 
 }
+const cars = [
+    { label: 'BMW'},
+    { label: 'i8'},
+    { label: 'Aqua'},
+    { label: 'Bens'},
+]
 
 export default PlaceRequest
