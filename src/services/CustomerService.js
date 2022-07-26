b52220eb6ccfca71f7a2e377345e600058223e80
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-07-26 10:08:49 +0530

    update CustomerService.js

---
+++ src/services/CustomerService.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+class CustomerService {
+    baseURL = "http://localhost:8080/backEnd/api/v1/";
+
+    addCustomer = async (data) => {
+        const promise = new Promise((resolve, reject) => {
+            axios.post(`${this.baseURL}admin/customer`, data)
+                .then((res) => {
+                    return resolve(res)
+                })
+                .catch((er) => {
+                    return resolve(er)
+                })
+        })
+        return await promise
+    }
+
+    putCustomer = async (data) => {
+        const promise = new Promise((resolve, reject) => {
+            axios.put(`${this.baseURL}admin/customer`, data, {
+            })
+                .then((res) => {
+
+                    return resolve(res)
+                })
+                .catch((err) => {
+                    return resolve(err)
+                })
+        })
+        return await promise;
+    };
+
+    fetchCustomer = async () => {
+        const promise = new Promise((resolve, reject) => {
+            axios.get(`${this.baseURL}admin/customer`)
+                .then((res) => {
+                    return resolve(res)
+                })
+                .catch((err) => {
+                    return resolve(err)
+                })
+        })
+        return await promise;
+    }
+
+}
+
+export default new CustomerService()
